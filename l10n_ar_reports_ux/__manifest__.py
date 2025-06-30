52c6e190bc1766fd8e21751c454d71a02f396181
Juan Pablo Garza <jgarza@juanpgarza.com>
2025-06-30 10:26:07 +0000

    [MIG] l10n_ar_reports_ux: Migration to 18.0

--- l10n_ar_reports_ux/__manifest__.py
+++ l10n_ar_reports_ux/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "l10n_ar_reports_ux",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/juanpgarza/account-ee-addons",
     "author": "juanpgarza",
@@ -14,5 +14,5 @@
     "data": 
         [
         ],
-    "installable": False,
+    "installable": True,
 }
